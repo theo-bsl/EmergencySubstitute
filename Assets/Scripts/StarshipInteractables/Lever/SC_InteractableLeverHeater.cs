542a53252b19289bfc184b12e1c735540958db09
xinom4966 <sd.dessay@gmail.com>
2024-04-25 17:26:38 +0200

    Tried to change the interactable for it to have 3 states

--- Assets/Scripts/StarshipInteractables/Lever/SC_InteractableLeverHeater.cs
+++ Assets/Scripts/StarshipInteractables/Lever/SC_InteractableLeverHeater.cs
@@ -6,11 +6,16 @@ public class SC_InteractableLeverHeater : SC_InteractableLever
 
     private Transform m_transform;
 
+    private Transform m_baseTransform;
+
     private Quaternion m_deltaRotation;
+
+    private float m_state;
 
     private void Awake()
     {
         m_transform = transform;
+        m_baseTransform = m_transform;
     }
 
     public override void OnSelected()
@@ -30,6 +35,25 @@ public class SC_InteractableLeverHeater : SC_InteractableLever
             m_dragDirection.x = DragDist.x;
             m_deltaRotation = Quaternion.Euler(0, m_dragDirection.x, 0);
             m_transform.rotation =  m_transform.rotation * m_deltaRotation;
+            if (m_deltaRotation.x > 0)
+            {
+                m_state += m_deltaRotation.y;
+            }
+            else if (m_deltaRotation.x < 0)
+            {
+                m_state -= m_deltaRotation.y;
+            }
+        }
+        if (m_state == 0)
+        {
+            m_transform.rotation = m_baseTransform.rotation;
+        }
+        else if (m_state > 0)
+        {
+            SC_StarshipManager.Instance.ChangeTemperature(m_dragDirection.y);
+        }
+        else if (m_state < 0)
+        {
             SC_StarshipManager.Instance.ChangeTemperature(m_dragDirection.y);
         }
     }
