514623e3c977791195d2c01f72939028d6a7343f
xinom4966 <sd.dessay@gmail.com>
2024-05-02 17:28:37 +0200

    Should be finished with the freeze and fog but needs testing

--- Assets/Scripts/UI/SC_GlassEffects.cs
+++ Assets/Scripts/UI/SC_GlassEffects.cs
@@ -4,8 +4,11 @@ using UnityEngine.UI;
 public class SC_GlassEffects : MonoBehaviour
 {
     [SerializeField] private Image m_leftScreen;
+    private Color m_leftScreenColor;
     [SerializeField] private Image m_rightScreen;
+    private Color m_rightScreenColor;
     [SerializeField] private Image m_frontScreen;
+    private Color m_frontScreenColor;
 
     [SerializeField] private List<Sprite> m_freezeLeft;
     [SerializeField] private List<Sprite> m_freezeRight;
@@ -24,6 +27,9 @@ public class SC_GlassEffects : MonoBehaviour
         m_eventManager.NewEvent.AddListener(StartEffects);
         m_eventManager.DeleteEvent.AddListener(CancelEffects);
         m_starshipManager = SC_StarshipManager.Instance;
+        m_leftScreenColor = m_leftScreen.color;
+        m_rightScreenColor = m_rightScreen.color;
+        m_frontScreenColor = m_frontScreen.color;
     }
 
     private void StartEffects(SC_Event Event)
@@ -33,17 +39,26 @@ public class SC_GlassEffects : MonoBehaviour
             m_leftScreen.sprite = m_fogLeft[0];
             m_rightScreen.sprite = m_fogRight[0];
             m_frontScreen.sprite = m_fogFront[0];
+            m_leftScreenColor.a = 1;
+            m_rightScreenColor.a = 1;
+            m_frontScreenColor.a = 1;
         }
         else if (Event.Name == "manque de chaleur")
         {
             m_leftScreen.sprite = m_freezeLeft[0];
             m_rightScreen.sprite = m_freezeRight[0];
             m_frontScreen.sprite = m_freezeFront[0];
+            m_leftScreenColor.a = 1;
+            m_rightScreenColor.a = 1;
+            m_frontScreenColor.a = 1;
         }
     }
 
     private void CancelEffects(SC_Event Event)
     {
+        m_leftScreenColor.a = 0;
+        m_rightScreenColor.a = 0;
+        m_frontScreenColor.a = 0;
         m_leftScreen.sprite = null;
         m_rightScreen.sprite = null;
         m_frontScreen.sprite = null;
